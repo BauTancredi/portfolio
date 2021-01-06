295eff0c956423101fe5697b0a38d501925f4c01
Bautista Tancredi <bautistatancredi@hotmail.com>
2021-01-06 17:50:36 -0300

    Creating the About Me section (WIP)

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,11 @@ import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { Button, Grid, Typography, useMediaQuery } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  mainContainer: {},
+  mainContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   heroContainer: {
     height: "100vh",
     [theme.breakpoints.down("sm")]: {
@@ -38,6 +42,14 @@ const useStyles = makeStyles((theme) => ({
         transform: "scale(1.2)",
         transition: "transform 330ms ease-in-out",
       },
+    },
+  },
+  monitor: {
+    width: "250%",
+    height: "250%",
+    [theme.breakpoints.down("lg")]: {
+      width: "200%",
+      height: "200%",
     },
   },
 }));
@@ -116,7 +128,13 @@ export default function Index() {
         </Grid>
       </Grid>
       {/* Recent Work */}
-      <Grid item container direction="column" alignItems="center">
+      <Grid
+        item
+        container
+        direction="column"
+        alignItems="center"
+        style={{ width: "90vw" }}
+      >
         <Grid item>
           <Typography
             variant="h2"
@@ -157,6 +175,63 @@ export default function Index() {
           </Grid>
           <Grid item container sm={6} md={4} justify="center">
             <div className={classes.projectContainer}></div>
+          </Grid>
+        </Grid>
+      </Grid>
+      {/* About Me */}
+      <Grid
+        item
+        container
+        style={{ width: "90vw", margin: "auto", marginTop: "5em" }}
+      >
+        <Grid item container direction="column" md>
+          <Grid item>
+            <Typography
+              variant="h2"
+              style={{
+                fontSize: matchesSM ? "2.2rem" : undefined,
+                marginBottom: "1.5em",
+              }}
+            >
+              About Me
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="body1"
+              style={{
+                maxWidth: matchesXS ? "15em" : "30em",
+                fontSize: matchesSM ? "1.5rem" : undefined,
+                marginTop: matchesSM ? "1.5em" : undefined,
+              }}
+              paragraph
+              gutterBottom
+            >
+              I am passionate about technology. I love designing incredible
+              websites to later bring them to reality.
+            </Typography>
+            <Typography
+              variant="body1"
+              style={{
+                maxWidth: matchesXS ? "15em" : "30em",
+                fontSize: matchesSM ? "1.5rem" : undefined,
+              }}
+              paragraph
+              gutterBottom
+            >
+              My toolbox for work consists of the following: Javascript, React,
+              Next.js, Material-UI, Node.js, mongoDB, Firebase and Google
+              Analytics.
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item container md justify="center" alignItems="center">
+          <Grid item>
+            <img
+              src="/assets/monitor.svg"
+              alt="monitor"
+              className={classes.monitor}
+            />
           </Grid>
         </Grid>
       </Grid>
